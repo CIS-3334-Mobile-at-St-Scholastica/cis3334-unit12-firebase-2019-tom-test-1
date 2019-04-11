09c6caa07dcab99524a40d59113ea0b3309cdfe0
Tom Gibbons <tgibbons@css.edu>
2019-04-11 13:16:42 -0500

    In class updates

--- app/src/main/java/edu/css/cis3334_unit12_firebase_2019/ChatActivity.java
+++ app/src/main/java/edu/css/cis3334_unit12_firebase_2019/ChatActivity.java
@@ -1,11 +1,19 @@
 package edu.css.cis3334_unit12_firebase_2019;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -13,6 +21,7 @@ public class ChatActivity extends AppCompatActivity {
     Button btnLogout;
     EditText etMessage;
     TextView tvMsgList;
+    DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +33,45 @@ public class ChatActivity extends AppCompatActivity {
         etMessage = findViewById(R.id.editTextMessage);
         tvMsgList = findViewById(R.id.textViewMsgList);
 
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("part12firebaseauth");
+
+        String key = myRef.push().getKey();
+        myRef.child(key).setValue("FIRST test message");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //String msg = dataSnapshot.getValue(String.class);
+                Toast.makeText(getApplicationContext(), "onDataChange called !!! ", Toast.LENGTH_LONG).show();
+
+                tvMsgList.setText("");           // clear out the all messages on the list
+                for (DataSnapshot msgSnapshot : dataSnapshot.getChildren()) {
+                    String msg = msgSnapshot.getValue(String.class);
+                    tvMsgList.setText(msg+ "\n" + tvMsgList.getText());
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
         btnPost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "btnPost.setOnClickListener called !!! ", Toast.LENGTH_LONG).show();
+
                 String msgText = etMessage.getText().toString();
                 etMessage.setText("");           // clear out the message text box to be ready for the next message
 
+                // ---- Get a new database key for the vote
+                String key = myRef.push().getKey();
+                Toast.makeText(getApplicationContext(), "btnPost.setOnClickListener key = "+key, Toast.LENGTH_LONG).show();
 
-                // This does not push message to firebase yet...
-                //    The next line should really be in the Firebase onDataChange() method
-                tvMsgList.setText(msgText+ "\n" + tvMsgList.getText());
-
+                // ---- write the message to Firebase
+                myRef.child(key).setValue(msgText);
             }
         });
 
--- app/src/main/java/edu/css/cis3334_unit12_firebase_2019/MainActivity.java
+++ app/src/main/java/edu/css/cis3334_unit12_firebase_2019/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.css.cis3334_unit12_firebase_2019;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +10,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,12 +27,14 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonCreateLogin;
     private Button buttonSignOut;
     private Button buttonStartChat;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
         textViewStatus = (TextView) findViewById(R.id.textViewStatus);
         editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
@@ -74,22 +83,84 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+        if (currentUser != null) {
+            // User is signed in
+            Log.d("CIS3334", "onAuthStateChanged:signed_in:" + currentUser.getUid());
+            Toast.makeText(MainActivity.this, "User Signed In", Toast.LENGTH_LONG).show();
+            textViewStatus.setText("Signed In");
+        } else {
+            // User is signed out
+            Log.d("CIS3334", "onAuthStateChanged:signed_out");
+            Toast.makeText(MainActivity.this, "User Signed Out", Toast.LENGTH_LONG).show();
+            textViewStatus.setText("Signed Out");
+        }
+    }
+
     private void createAccount(String email, String password) {
 
         Toast.makeText(getApplicationContext(), "Create Account not implemented yet !!! ", Toast.LENGTH_LONG).show();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("CIS3334", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                            textViewStatus.setText("Status: Logged in!!!");
 
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("CIS3334", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                            textViewStatus.setText("Status: Logged OUT");
+                        }
+
+                        // ...
+                    }
+                });
     }
 
     private void signIn(String email, String password){
 
         Toast.makeText(getApplicationContext(), "signIn not implemented yet !!! ", Toast.LENGTH_LONG).show();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("CIS3334", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                            textViewStatus.setText("Status: Logged in!!!");
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("CIS3334", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                            textViewStatus.setText("Status: Logged OUT");
+                        }
 
+                        // ...
+                    }
+                });
     }
 
     private void signOut () {
 
         Toast.makeText(getApplicationContext(), "signOut not implemented yet !!! ", Toast.LENGTH_LONG).show();
-
+        mAuth.signOut();
     }
 
     private void googleSignIn() {
